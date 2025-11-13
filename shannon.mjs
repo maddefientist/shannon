3b27e06943c677be5cab48c2b01844505c95d8ea
Khaushik-keygraph <khaushik.contractor@keygraph.io>
2025-11-13 20:33:58 +0530

    fix: removed comments

--- shannon.mjs
+++ shannon.mjs
@@ -1,8 +1,9 @@
+#!/usr/bin/env zx
+
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-#!/usr/bin/env zx
 
 import { path, fs } from 'zx';
 import chalk from 'chalk';
